62fdc32b2efafc3598f4b943122f2db2459334e3
dishitago <goswamidishita93@gmail.com>
2026-02-08 22:37:46 +0530

    Refactor isodd method to main and adjust output
    Refactored the isodd method into the main method and updated output.

---
+++ Is Odd?.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class merttle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input1 = sc.nextInt();
+        if(input1%2!=0){
+            System.out.println("2");
+        }else{
+            System.out.println("1");
+        }
+    }
+}
