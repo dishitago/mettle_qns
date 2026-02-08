2fd9e17905b7e1cc1403d5f37c66bda874f657dd
dishitago <goswamidishita93@gmail.com>
2026-02-08 22:55:29 +0530

    Update and rename Return second last digit of given number to Return second last digit of given number.java

--- Return second last digit of given number.java
+++ Return second last digit of given number.java
@@ -1,8 +1,7 @@
-import java.io.*;
-import java.util.*; 
-class UserMainCode
-{
-   public static void main(String[] args) {
+import java.util.Scanner;
+
+public class merttle {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter input1:");
         int input1 = sc.nextInt();
