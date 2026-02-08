a2e4ba22a30cf5a7e6ad9e53e397c0eab66e5959
dishitago <goswamidishita93@gmail.com>
2026-02-08 22:53:54 +0530

    Update and rename Return last digit of the given number to Return last digit of the given number.java

--- Return last digit of the given number.java
+++ Return last digit of the given number.java
@@ -1,8 +1,8 @@
-import java.io.*;
-import java.util.*; 
-class UserMainCode {
- public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+import java.util.Scanner;
+
+public class merttle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         System.out.println("enter input1:");
         int input1 = sc.nextInt();
         int num=Math.abs(input1);
